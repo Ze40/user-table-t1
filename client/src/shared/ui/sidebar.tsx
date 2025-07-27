262f02fec432d4c45177b16b981c3fdc4861f666
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-27 12:39:03 +0300

    fix: sidebar size in closed state

--- client/src/shared/ui/sidebar.tsx
+++ client/src/shared/ui/sidebar.tsx
@@ -17,7 +17,7 @@ const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 const SIDEBAR_WIDTH = "16rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
-const SIDEBAR_WIDTH_ICON = "3rem";
+const SIDEBAR_WIDTH_ICON = "4rem";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
 
 type SidebarContextProps = {
@@ -683,5 +683,6 @@ export {
   SidebarRail,
   SidebarSeparator,
   SidebarTrigger,
+  // eslint-disable-next-line react-refresh/only-export-components
   useSidebar,
 };

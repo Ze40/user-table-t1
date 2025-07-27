e8659033f20848735f3e4afb8572aae71fdfc84b
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-27 11:47:48 +0300

    chore: add sidebar from shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { Layout } from "./pages/main";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter basename="user-table-t1">
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/widgets/app-sidebar.tsx
@@ -0,0 +1,5 @@
+const AppSidebar = () => {
+  return <></>;
+};
+
+export default AppSidebar;

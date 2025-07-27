81640fb7863120ba5828ef94e97c0d6d98814dd0
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-27 11:32:53 +0300

    chore: init project with ready back

---
+++ client/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  base: "user-table-t1",
+  server: {
+    allowedHosts: ["localhost"],
+    port: 5173,
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        secure: false,
+        target: "http://localhost:4000",
+      },
+    },
+    strictPort: true,
+  },
+});
